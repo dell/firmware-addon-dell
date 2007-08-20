11a05563e0499bda9f59fd4fea7f785d1cfff0ee
Michael E Brown <mebrown@michaels-house.net>
2007-08-20 00:44:04 -0500

    version bump

--- yum-plugin/dellsysidplugin.py
+++ yum-plugin/dellsysidplugin.py
@@ -10,7 +10,7 @@ from firmware_addon_dell.biosHdr import getSystemId, getServiceTag
 
 from yum.plugins import TYPE_CORE
 
-version="1.4.6"
+version="1.4.7"
 
 requires_api_version = '2.1'
 plugin_type = TYPE_CORE
