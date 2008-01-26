56d32a22bc1486dbdaf4600ed477eb83cfd23164
Michael E Brown <mebrown@michaels-house.net>
2008-01-25 23:48:33 -0600

    add LIBEXECDIR def so other modules can use it.

--- firmware_addon_dell/__init__.py
+++ firmware_addon_dell/__init__.py
@@ -1,2 +1,8 @@
 
+import os
+
+def mkselfrelpath(*args):
+    return os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), *args))
+
 __VERSION__="unreleased version"
+LIBEXECDIR=mkselfrelpath("..", "libexec")
